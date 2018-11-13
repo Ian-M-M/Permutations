aaa2a14c869104ef4513b29f4a138769508b618d
MarcosJSP <32165693+MarcosJSP@users.noreply.github.com>
2018-11-13 15:46:11 +0000

    +GetParameters

--- Pr3/Java/src/Max/Main.java
+++ Pr3/Java/src/Max/Main.java
@@ -5,28 +5,39 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.math.BigInteger;
 import java.text.DecimalFormat;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException, IOException{        
-        Main clase= new Main();        
-        String path = "../Permutaciones.txt";
+
+
+
+    
+    
+    public static void main(String[] args) throws FileNotFoundException, IOException{
+
+        Main clase= new Main();
+        
+        GetParam(args);
+        
+        if(di_flag)Input();
+        if(!do_flag && !t_flag)System.exit(0);
         FileReader fichero = new FileReader(path);
         BufferedReader br = new BufferedReader(fichero);// cargamos el fichero en un buffer
         String linea="";
 
         while((linea = br.readLine()) != null){
             String [] elementos = linea.split(",");//elementos obtenidos del fichero
-            ComienzaTimer();//Comenzamos a contar el tiempo de ejecucion
+            if(t_flag)ComienzaTimer();//Comenzamos a contar el tiempo de ejecucion
             long maximo = clase.BruteForce(elementos);//iterador
-            ParaTimer();//Calculamos el tiempo de ejecucion
-            Output(elementos, maximo);//info a imprimir por pantalla
+            if(t_flag)ParaTimer();//Calculamos el tiempo de ejecucion
+            if(do_flag)Output(elementos, maximo);//info a imprimir por pantalla
+            if(t_flag)T_Output();
         }
         br.close();
-    }    
+    }
     
-    public long BruteForce (String [] elementos){
+    
+        public long BruteForce (String [] elementos){
         int[] indices; // orden a mostrar de elementos
         Permutator p = new Permutator (elementos.length);
         long maximo = 0;
@@ -38,5 +49,5 @@ public class Main {
             }
         }
         return maximo;
-    } 
+    }
 }
--- Pr3/Java/src/Max/etc.java
+++ Pr3/Java/src/Max/etc.java
@@ -1,21 +1,61 @@
 package Max;
-
-import java.math.BigInteger;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.text.DecimalFormat;
+import javax.sound.midi.SysexMessage;
 
 public class etc {
+    
+    // POINTERS ////////////////////////////////////////////////////////////////
+    public static boolean f_flag=false, t_flag=false, di_flag=false, do_flag=false;
+    public static String path="";
+    
+    public static void GetParam(String [] args){
+        if (args.length==0) System.exit(1);        
+        for (int i = 0; i < args.length; i++) {
+            if(args[i].equals("-f")){
+                f_flag=true;
+                path=args[i+1];
+            }
+            if(args[i].equals("-t"))t_flag=true;
+            if(args[i].equals("-di"))di_flag=true;
+            if(args[i].equals("-do"))do_flag=true;           
+        }
+        if (!f_flag){
+            System.err.println("Ha de introducir un fichero");
+            System.exit(1);
+        }
+    }
+    
+    // INPUT ///////////////////////////////////////////////////////////////////
+    public static void Input(){
+        try{
+            System.out.println(path);
+            FileReader fichero = new FileReader(path);
+            BufferedReader br = new BufferedReader(fichero);// cargamos el fichero en un buffer
+            String linea="";
+            while((linea = br.readLine()) != null){
+                System.out.println(linea);
+            }
+            br.close();
+            System.out.println("-------------------------------------------------");
+        }catch (Exception e){
+            System.err.println("Error al leer input");
+        }        
+    }    
     
     // TIMERS //////////////////////////////////////////////////////////////////
     
     private static long timer;
     private static double tiempoTranscurrido;
     
+    
     public static void ComienzaTimer(){
         timer = System.nanoTime();
     }
     
     public static void ParaTimer(){
-        tiempoTranscurrido = (System.nanoTime() - timer)/(1000000000.);     
+        tiempoTranscurrido = (System.nanoTime() - timer)/(1000000000.);
     }
     
     // LST OPERATIONS //////////////////////////////////////////////////////////
@@ -29,19 +69,26 @@ public class etc {
     }
     
     // OUTPUT //////////////////////////////////////////////////////////////////
+    public static int contador=1;//nº de veces que se llama al timer
     
     public static void Output(String [] elementos, long maximo){
         System.out.print("Elementos [" + elementos.length + "] => { ");
         for (int i = 0; i < elementos.length; i++) {
             System.out.print(elementos[i] + " ");            
         }
-        System.out.println("}");
-        
-        DecimalFormat df = new DecimalFormat("#.#########");
-        System.out.println("Tiempo => " + df.format(tiempoTranscurrido) + "s");
-        
+        System.out.println("}");        
         System.out.println("Maxima permutacion => " + maximo);
-        System.out.println("-------------------------------------------------");  
+        if(!t_flag)System.out.println("-------------------------------------------------");  
+    }
+    
+    public static void T_Output(){
+        DecimalFormat df = new DecimalFormat("#.#########");
+        if(!do_flag){
+            System.out.print(contador + ".");
+            contador++;
+        }
+        System.out.println("Tiempo => " + df.format(tiempoTranscurrido) + "s");
+        System.out.println("-------------------------------------------------");
     }
     
 }
